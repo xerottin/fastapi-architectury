07a69b5b8a470adb12895142a5930406637b4d2f
xerottin <xerottin@gmail.com>
2026-03-24 18:18:55 +0500

    feat: file upload in minio

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -9,4 +9,5 @@ router_v1 = APIRouter()
 
 router_v1.include_router(user.router, prefix="/users", tags=["User"])
 router_v1.include_router(project.router, prefix="/project", tags=["Project"])
+router_v1.include_router(file.router, prefix="/file", tags=["File"])
 router_v1.include_router(mongo_test.router, prefix="/mongo_test", tags=["Mongo-Test"])
